ea613193f5b2321959c6013d4375bbf76250eb70
tracer-pa2018-fall <tracer@njuics.org>
2018-09-16 19:38:40 +0800

    >  nemu 151242031 mwq Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  19:38:40 up  6:52,  1 user,  load average: 0.11, 0.05, 0.01 d55eab0d50410a9dd9d3d9b27b8dfc8c3de5b3d5

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -221,7 +221,7 @@ uint32_t alu_and(uint32_t src, uint32_t dest, size_t data_size) {
   cpu.eflags.OF = 0;
 	//printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
 	//assert(0);
-	return 0;
+	return result;
 #endif
 }
 
