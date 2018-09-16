cb8ac114e2e27463e926d57e536328fdf28576aa
tracer-pa2018-fall <tracer@njuics.org>
2018-09-16 20:17:47 +0800

    >  nemu 151242031 mwq Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  20:17:47 up  7:31,  1 user,  load average: 0.12, 0.07, 0.02 fe7a1fb65fe79003b6d855bd706fa99b8b9832d7

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -275,7 +275,7 @@ uint32_t alu_shl(uint32_t src, uint32_t dest, size_t data_size) {
   set_PF(res);
   set_ZF(res,data_size);
   set_SF(res,data_size);
-  return res&(0xffffffff>>(32-data_size))
+  return res&(0xffffffff>>(32-data_size));
   //printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
 	//assert(0);
 	//return 0;
