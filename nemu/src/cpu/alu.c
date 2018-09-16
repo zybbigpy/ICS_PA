2bb9f45d17fae2739833ada0bc3fad0c4e2be800
tracer-pa2018-fall <tracer@njuics.org>
2018-09-16 17:51:25 +0800

    >  nemu 151242031 mwq Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  17:51:25 up  5:50,  1 user,  load average: 0.18, 0.16, 0.06 30df9762192c51c3a35a72f1b8469eb15bed0136

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -127,7 +127,7 @@ uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size) {
   set_CF_sub(res,dest,data_size);
   set_ZF(res,data_size);
   set_PF(res);
-  //set_SF(res,data_size);
+  set_SF(res,data_size);
   //OF is the same as add 
   //set_OF_sub(res,src,dest,data_size);
   //	alu_mulssert(0);
