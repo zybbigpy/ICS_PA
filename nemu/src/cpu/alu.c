b24315147bb38774924e34a07a6297429975fc1b
tracer-pa2018-fall <tracer@njuics.org>
2018-09-17 09:48:37 +0800

    >  nemu 151242031 mwq Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  09:48:37 up 12 min,  1 user,  load average: 0.15, 0.12, 0.09 9af9b84034b95d84b1937716b0bfcb4dcc10031a

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -7,13 +7,13 @@ void set_CF_add(uint32_t result,uint32_t src,size_t data_size){
 }
 
 void set_CF_adc(uint32_t result,uint32_t src,size_t data_size){
-  if(cpu.eflags == 0)
+  if(cpu.eflags.CF == 0)
   {
     result = sign_ext(result&(0xffffffff>>(32-data_size)),data_size);
     src = sign_ext(src&(0xffffffff>>(32-data_size)),data_size);
     cpu.eflags.CF = result < src;
   }
-  else if(cpu.eflags == 1)
+  else if(cpu.eflags.CF == 1)
   {
     result = sign_ext(result&(0xffffffff>>(32-data_size)),data_size);
     src = sign_ext(src&(0xffffffff>>(32-data_size)),data_size);
