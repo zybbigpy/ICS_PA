531fa2806c8543b127979669039a6743bc2702bd
tracer-pa2018-fall <tracer@njuics.org>
2018-10-11 20:48:00 +0800

    >  clean 151242031 mwq Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  20:48:00 up 12:42,  1 user,  load average: 0.02, 0.03, 0.00 720070b541df8138dc4d6aeccc3517bc4d6489

--- nemu/include/cpu/instr/push.h
+++ nemu/include/cpu/instr/push.h
@@ -1,6 +1,6 @@
 #ifndef __INSTR_PUSH_H__
 #define __INSTR_PUSH_H__
 
-make_instr_func(push_r_l);
+make_instr_func(push_r_v);
 
 #endif 
--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -2,11 +2,11 @@
 
 static void instr_execute_1op() {
   operand_read(&opr_src);
-  cpu.esp -= 4;
+  cpu.esp -= opr_src.data_size/8;
   opr_dest.addr = cpu.esp ;
   opr_dest.type = OPR_MEM;
   opr_dest.val = opr_src.val;
   operand_write(&opr_dest);
 }
 
-make_instr_impl_1op(push, r, l)
+make_instr_impl_1op(push, r, v)
