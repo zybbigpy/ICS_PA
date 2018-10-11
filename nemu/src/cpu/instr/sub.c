4cd5c90d5445f36ad91a9fd0031735c017eb8d5c
tracer-pa2018-fall <tracer@njuics.org>
2018-10-11 20:57:10 +0800

    >  clean 151242031 mwq Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  20:57:10 up 12:51,  1 user,  load average: 0.15, 0.10, 0.04 76b9508c220a7313aa54e7c38e8cd3f3166ca45

--- nemu/src/cpu/instr/sub.c
+++ nemu/src/cpu/instr/sub.c
@@ -3,6 +3,8 @@
 static void instr_execute_2op() {
   operand_read(&opr_src);
   operand_read(&opr_dest);
+  printf("opr_src-val%x",opr_src.val);
+  printf("opr_dest-val%x",opr_dest.val);
   opr_dest.val = alu_sub(opr_src.val,opr_src.val,opr_dest.data_size);
   operand_write(&opr_dest);
 }
