1bb95983abf370a39d0d4365230d2495bcc9f7f1
monitor <tracer@njuics.org>
2018-10-11 12:43:01 +0800

    monitor test

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -21,7 +21,7 @@ extern uint8_t data_size;
 #include "cpu/instr/group.h"
 #include "cpu/instr/special.h"
 #include "cpu/instr/x87.h"
-
+#include "cpu/instr/push.h"
 /* TODO: add more instructions here */
 
 #endif
