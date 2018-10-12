28953f9e4e03fa636bbd1f52aaafbd303be2acf8
monitor <tracer@njuics.org>
2018-10-12 21:34:01 +0800

    monitor test

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -5,10 +5,10 @@ make_instr_func(ret_near)
     //pop eip
     OPERAND help;
     help.type = OPR_MEM;
-    help.data_size = 32; //??
+    help.data_size = 32; //??s
     help.addr = cpu.esp;
     operand_read(&help);
     cpu.eip = help.val;
-    cpu.esp +=4;
-    return 1+data_size/8;//len?=0s
+    cpu.esp += 4;
+    return 1 + data_size / 8; //len?=0s
 }
