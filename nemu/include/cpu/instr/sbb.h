5bbcff3eaf799f76108af59559eaf9079ee087c0
monitor <tracer@njuics.org>
2018-10-12 20:34:01 +0800

    monitor test

--- nemu/include/cpu/instr/sbb.h
+++ nemu/include/cpu/instr/sbb.h
@@ -2,5 +2,5 @@
 #define __INSTR_SBB_H__
 
 make_instr_func(sbb_i2rm_bv);
-
+make_instr_func(sbb_i2rm_v);
 #endif
--- nemu/include/cpu/instr/xor.h
+++ nemu/include/cpu/instr/xor.h
@@ -2,5 +2,5 @@
 #define __INSTR_XOR_H__
 
 make_instr_func(xor_i2rm_bv);
-
+make_instr_func(xor_i2rm_v);
 #endif
