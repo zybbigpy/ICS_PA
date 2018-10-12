e60c85513fe9f0d74ed3ee2b28ea13746f569cdb
monitor <tracer@njuics.org>
2018-10-12 19:33:02 +0800

    monitor test

---
+++ nemu/src/cpu/instr/call.c
@@ -0,0 +1,18 @@
+#include "cpu/instr.h"
+make_instr_func(jmp_near) {
+        OPERAND rel;
+        rel.type = OPR_IMM;
+	rel.sreg = SREG_CS;
+        rel.data_size = data_size;
+        rel.addr = eip + 1;
+
+        operand_read(&rel);
+        printf("jmp_near val is 0x%x/n",rel.val);
+	int offset = sign_ext(rel.val, data_size);
+	print_asm_1("jmp", "", 2, &rel);
+        printf("offset is %d/n",offset);
+	cpu.eip += offset;
+        printf("eip is 0x%x\n",cpu.eip);
+
+        return 1 + data_size / 8;
+}
