1603301da70ff9bf729d58a25d4d1d20bf8158a9
monitor <tracer@njuics.org>
2018-10-12 16:34:01 +0800

    monitor test

---
+++ nemu/src/cpu/instr/cmp.c
@@ -0,0 +1,13 @@
+#include "cpu/instr.h"
+
+static void instr_execute_2op() {
+  operand_read(&opr_src);
+  operand_read(&opr_dest);
+  printf("opr_src-val is:  0x%x\n",opr_src.val);
+  printf("opr_dest-val is: 0x%x\n",opr_dest.val);
+  opr_dest.val = alu_sub(opr_src.val,opr_dest.val,opr_dest.data_size);
+  printf("after sub opr_dest-val is: 0x%x\n",opr_dest.val);
+  operand_write(&opr_dest);
+}
+
+make_instr_impl_2op(cmp, i, rm, bv)
