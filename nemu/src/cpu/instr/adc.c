4ca90a5bfaa8909dbff6b627843057ce8e85a5e2
monitor <tracer@njuics.org>
2018-10-12 20:39:01 +0800

    monitor test

--- nemu/src/cpu/instr/adc.c
+++ nemu/src/cpu/instr/adc.c
@@ -1,14 +1,15 @@
 #include "cpu/instr.h"
 
-static void instr_execute_2op() {
+static void instr_execute_2op()
+{
   operand_read(&opr_src);
   operand_read(&opr_dest);
-  printf("opr_src-val is:  0x%x\n",opr_src.val);
-  printf("opr_dest-val is: 0x%x\n",opr_dest.val);
-  opr_dest.val = alu_adc(opr_src.val,opr_dest.val,opr_dest.data_size);
-  printf("after adc opr_dest-val is: 0x%x\n",opr_dest.val);
+  printf("opr_src-val is:  0x%x\n", opr_src.val);
+  printf("opr_dest-val is: 0x%x\n", opr_dest.val);
+  opr_dest.val = alu_adc(opr_src.val, opr_dest.val, opr_dest.data_size);
+  printf("after adc opr_dest-val is: 0x%x\n", opr_dest.val);
   operand_write(&opr_dest);
 }
 
 make_instr_impl_2op(adc, i, rm, bv)
-make_instr_impl_2op(adc,i,rm,v)
+make_instr_impl_2op(adc, i, rm, v)
--- nemu/src/cpu/instr/xor.c
+++ nemu/src/cpu/instr/xor.c
@@ -11,3 +11,4 @@ static void instr_execute_2op() {
 }
 
 make_instr_impl_2op(xor, i, rm, bv)
+make_instr_impl_2op(xor, i, rm, v)
