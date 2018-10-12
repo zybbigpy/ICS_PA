f294c0bec4f495d48b0a9e71736e86b1ae411d61
monitor <tracer@njuics.org>
2018-10-12 16:06:01 +0800

    monitor test

---
+++ nemu/include/cpu/instr/adc.h
@@ -0,0 +1,6 @@
+#ifndef __INSTR_ADC_H__
+#define __INSTR_ADC_H__
+
+make_instr_func(adc_i2rm_bv);
+
+#endif
--- nemu/include/cpu/instr/add.h
+++ nemu/include/cpu/instr/add.h
@@ -1,6 +1,6 @@
 #ifndef __INSTR_ADD_H__
 #define __INSTR_ADD_H__
 
-make_instr_func(sub_i2rm_bv);
+make_instr_func(add_i2rm_bv);
 
 #endif
