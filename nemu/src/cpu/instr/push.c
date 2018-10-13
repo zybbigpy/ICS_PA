9724a604937e634148795548f6dd2a2d65db4376
tracer-pa2018-fall <tracer@njuics.org>
2018-10-13 11:47:49 +0800

    >  clean 151242031 mwq Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  11:47:49 up  5:41,  1 user,  load average: 0.09, 0.16, 0.17 f4df09f3802e232c05f6e1f1d7908ce68ffdd59

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -1,10 +1,12 @@
 #include "cpu/instr.h"
 
-static void instr_execute_1op() {
+static void instr_execute_1op()
+{
   OPERAND help;
   operand_read(&opr_src);
-  cpu.esp -= opr_src.data_size/8;
-  help.addr = cpu.esp ;
+  //esp-4 or esp-2
+  cpu.esp -= opr_src.data_size / 8;
+  help.addr = cpu.esp;
   help.type = OPR_MEM;
   help.val = opr_src.val;
   operand_write(&help);
