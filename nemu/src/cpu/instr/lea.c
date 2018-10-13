ab729a1d8f6a7d965d1b97aab56a267d35084b9b
tracer-pa2018-fall <tracer@njuics.org>
2018-10-13 20:33:32 +0800

    >  nemu 151242031 mwq Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  20:33:32 up 10:19,  1 user,  load average: 0.40, 0.27, 0.23 2de62cc22ed7b81a09e669f4cbf01b8fde796b5

--- nemu/src/cpu/instr/lea.c
+++ nemu/src/cpu/instr/lea.c
@@ -1,2 +1,9 @@
 #include "cpu/instr.h"
 
+static void instr_execute_2op() {
+	operand_read(&opr_src);
+	opr_dest.val = opr_src.addr;
+	operand_write(&opr_dest);
+}
+
+make_instr_impl_2op(lea, rm, r, v)
