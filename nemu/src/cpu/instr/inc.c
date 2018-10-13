58cf981a54cb35ff7f73d8f9e4f9eda5d55eb2df
monitor <tracer@njuics.org>
2018-10-13 15:41:01 +0800

    monitor test

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -70,8 +70,13 @@ static void instr_execute_1op()
 {
     operand_read(&opr_src);
     opr_src.val += 1;
+    setZF(res, data_size);
+    setPF(res);
+    setSF(res, data_size);
+    setOF_add(res, src, dest, data_size);
+
     operand_write(&opr_src);
 }
 
 make_instr_impl_1op(inc, r, v)
-    make_instr_impl_1op(inc, rm, v)
+make_instr_impl_1op(inc, rm, v)
