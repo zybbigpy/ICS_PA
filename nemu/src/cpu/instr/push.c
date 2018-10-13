eda193be82efa8515ca2bcd7fd5c4f75a91a7b54
monitor <tracer@njuics.org>
2018-10-13 11:51:01 +0800

    monitor test

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -7,6 +7,7 @@ static void instr_execute_1op()
   //esp-4 or esp-2
   cpu.esp -= opr_src.data_size / 8;
   help.addr = cpu.esp;
+  help.data_size = opr_src.data_size;
   help.type = OPR_MEM;
   help.val = opr_src.val;
   operand_write(&help);
