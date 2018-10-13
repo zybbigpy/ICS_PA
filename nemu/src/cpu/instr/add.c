fe1b3fda8034e78fbf6f91e2f6d20a276a0a6f01
monitor <tracer@njuics.org>
2018-10-13 15:01:01 +0800

    monitor test

--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -12,3 +12,9 @@ static void instr_execute_2op() {
 
 make_instr_impl_2op(add, i, rm, bv)
 make_instr_impl_2op(add, i, rm, v)
+make_instr_impl_2op(add, r, rm, v)
+make_instr_impl_2op(add, r, rm, b)
+make_instr_impl_2op(add, rm, r, v)
+make_instr_impl_2op(add, rm, r, b)
+make_instr_impl_2op(add, i, a, b)
+make_instr_impl_2op(add, i, a, v)
