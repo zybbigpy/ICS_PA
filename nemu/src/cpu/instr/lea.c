59c7ae04f9877f2137033092d65d167b757dce99
monitor <tracer@njuics.org>
2018-10-13 20:32:01 +0800

    monitor test

--- nemu/src/cpu/instr/lea.c
+++ nemu/src/cpu/instr/lea.c
@@ -1,16 +1,2 @@
 #include "cpu/instr.h"
 
-make_instr_func(lea)
-{
-    OPERAND R;
-    OPERAND M;
-    R.data_size = M.data_size = data_size;
-    int len = 1;
-    len += modrm_r_rm(eip + 1, &R, &M);
-    operand_read(&M);
-    R.val = M.addr;
-    operand_write(&R);
-    print_asm_2("lea", "", len, &M, &R);
-
-    return len;
-}
