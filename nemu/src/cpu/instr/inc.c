ac021eac9256c71d6503441c33ca2537fe3d2886
tracer-pa2018-fall <tracer@njuics.org>
2018-10-13 19:30:20 +0800

    >  nemu 151242031 mwq Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  19:30:20 up  9:16,  1 user,  load average: 0.67, 0.53, 0.42 ef31e6b9fdffa45598a0b0333ab15091f3b104e5

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -1,14 +1,14 @@
 #include "cpu/instr.h"
 
-
 static void instr_execute_1op()
 {
     operand_read(&opr_src);
-    opr_dest.val = alu_add(1,opr_dest.val,opr_dest.data_size);
-
-
+    // reserve the CF flag
+    uint32_t CF_R = cpu.eflags.CF;
+    opr_dest.val = alu_add(1, opr_dest.val, opr_dest.data_size);
+    cpu.eflags.CF = CF_R;
     operand_write(&opr_src);
 }
 
 make_instr_impl_1op(inc, r, v)
-make_instr_impl_1op(inc, rm, v)
+    make_instr_impl_1op(inc, rm, v)
