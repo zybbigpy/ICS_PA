6c5cf8ef27b42fc02946dad29e423e078a529743
monitor <tracer@njuics.org>
2018-10-14 10:41:01 +0800

    monitor test

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -29,12 +29,12 @@ make_instr_func(jmp_short)
         rel.addr = eip + 1;
 
         operand_read(&rel);
-        printf("jmp_near val is 0x%x\n",rel.val);
+        //printf("jmp_near val is 0x%x\n",rel.val);
 	int offset = sign_ext(rel.val, 8);
 	print_asm_1("jmp", "", 2, &rel);
-        printf("offset is %d\n",offset);
+        //printf("offset is %d\n",offset);
 	cpu.eip += offset;
-        printf("eip is 0x%x\n",cpu.eip);
+        //printf("eip is 0x%x\n",cpu.eip);
 
         return 1 + 8 / 8;
 }
