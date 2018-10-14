dd3073225a2ddbc7aed30b8ce75aa50e56d56688
tracer-pa2018-fall <tracer@njuics.org>
2018-10-14 20:52:38 +0800

    >  nemu 151242031 mwq Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  20:52:38 up 20:45,  1 user,  load average: 0.97, 0.62, 0.45 a9fc66b7baa155946115ffa2a1017cb87d79210f

--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -10,3 +10,7 @@ static void instr_execute_2op() {
 
 make_instr_impl_2op(test, r, rm, b)
 make_instr_impl_2op(test, r, rm, v)
+make_instr_impl_2op(test, i, a, b)
+make_instr_impl_2op(test, i, a, v)
+make_instr_impl_2op(test, i, rm, b)
+make_instr_impl_2op(test, i, rm, v)
