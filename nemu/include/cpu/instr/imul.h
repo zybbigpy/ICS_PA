e1da096216562143d77d58024a54d56cb957a39e
monitor <tracer@njuics.org>
2018-10-14 16:52:01 +0800

    monitor test

---
+++ nemu/include/cpu/instr/imul.h
@@ -0,0 +1,7 @@
+#include "cpu/instr.h"
+
+make_instr_func(imul_rm2r_v);
+make_instr_func(imul_rm2a_b);
+make_instr_func(imul_rm2a_v);
+make_instr_func(imul_irm2r_v);
+make_instr_func(imul_i8rm2r_v);
