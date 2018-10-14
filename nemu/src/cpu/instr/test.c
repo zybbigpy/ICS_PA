906d78ab1804c94bf1fba17aeaf1b104fa9906ac
monitor <tracer@njuics.org>
2018-10-14 21:08:01 +0800

    monitor test

--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -2,9 +2,8 @@
 static void instr_execute_2op() {
   operand_read(&opr_src);
   operand_read(&opr_dest);
-  opr_dest.val = alu_and(opr_dest.val,opr_src.val,data_size);
+  alu_and(opr_dest.val,opr_src.val, opr_src.data_size);
   //printf("after and opr_dest-val is: 0x%x\n",opr_dest.val);
-  operand_write(&opr_dest);
 }
 
 make_instr_impl_2op(test, r, rm, b)
