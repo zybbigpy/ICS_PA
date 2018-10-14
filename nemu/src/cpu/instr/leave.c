81137c8478355627b99c04c47d17256713f4b6d2
monitor <tracer@njuics.org>
2018-10-14 09:45:01 +0800

    monitor test

--- nemu/src/cpu/instr/leave.c
+++ nemu/src/cpu/instr/leave.c
@@ -2,5 +2,14 @@
 
 make_instr_func(leave)
 {
-    
+    cpu.esp = cpu.ebp;
+    OPERAND help;
+    //pop ebp(old ebp value in stack)
+    help.addr = cpu.esp;
+    help.data_size = data_size;
+    help.type = OPR_MEM;
+    operand_read(&help);
+    cpu.ebp = help.val;
+    cpu.esp += data_size / 8;
+    return 1;
 }
