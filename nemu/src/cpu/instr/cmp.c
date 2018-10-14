23bed43e303d1ce5e7da0d32a7b6f45033989852
monitor <tracer@njuics.org>
2018-10-14 17:34:01 +0800

    monitor test

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -5,7 +5,8 @@ static void instr_execute_2op() {
   operand_read(&opr_dest);
   //printf("opr_src-val is:  0x%x\n",opr_src.val);
   //printf("opr_dest-val is: 0x%x\n",opr_dest.val);
-  alu_sub(opr_src.val,opr_dest.val,opr_dest.data_size);
+  alu_sub(sign_ext(opr_src.val, opr_src.data_size), \
+							opr_dest.val, opr_dest.data_size);
   //printf("EIP:0x %x\n",cpu.eip);
   //printf("after cmp opr_dest-val is: 0x%x\n",opr_dest.val);
 
