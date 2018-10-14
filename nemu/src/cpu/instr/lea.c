122205fc75b93090e36e41fbe8a8c2ed8f83671a
monitor <tracer@njuics.org>
2018-10-14 10:10:01 +0800

    monitor test

--- nemu/src/cpu/instr/lea.c
+++ nemu/src/cpu/instr/lea.c
@@ -6,7 +6,7 @@ make_instr_func(lea)
 	OPERAND rm;
 	r.data_size = rm.data_size = data_size;
 	int len = 1;
-	len += modrm_r_rm(cpu.eip + 1, &r, &rm);
+	len += modrm_r_rm(eip + 1, &r, &rm);
 	r.val = rm.addr;
 	operand_write(&r);
 	return len;
