48b4c92b37d41d4a37c29fcb0eeda1d291d88d59
monitor <tracer@njuics.org>
2018-11-13 20:50:01 +0800

    monitor test

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -1,7 +1,10 @@
 struct Cacheline {
     struct bitAndFlag {
+        //valid bit
         uint32_t validBit:1;
+        //flag
         uint32_t flag:19;
     };
+    // 64byte for content
     uint32_t content[16];
-}
+};
