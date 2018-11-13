dcb7d1fc9c554efa99e97c705fd4372a0ff7a947
monitor <tracer@njuics.org>
2018-11-13 20:57:01 +0800

    monitor test

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -11,5 +11,5 @@ struct CacheLine {
 };
 
 void init_cache();
-uint32_t cache_read(paddr_t paddr, size_t len, CacheLine * cache);
-void cache_write(paddr_t paddr, size_t len, uint32_t data, CacheLine * cache);
+uint32_t cache_read(paddr_t paddr, size_t len, CacheLine* cache);
+void cache_write(paddr_t paddr, size_t len, uint32_t data, CacheLine* cache);
