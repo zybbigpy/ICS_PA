579c23fb747cbf6f19c32d8e9ede3c8e6e0273bb
monitor <tracer@njuics.org>
2018-11-14 18:28:01 +0800

    monitor test

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -11,3 +11,12 @@ void init_cache() {
     // set all content especially validBit zero
     memset(L1_dcache, 0, sizeof(CacheLine)*1024);
 }
+
+uint32_t cache_read(paddr_t paddr, size_t len, CacheLine* cache) {
+    
+}
+
+// write cache
+void cache_write(paddr_t paddr, size_t len, uint32_t data, CacheLine* cache) {
+
+}
