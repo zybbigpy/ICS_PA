b6495aa47f7bfd61185a458eaa21884c542b4e0a
monitor <tracer@njuics.org>
2018-11-14 10:55:01 +0800

    monitor test

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -1,3 +1,6 @@
+#ifndef __CACHE_H__
+#define __CACHE_H__
+
 #include "nemu.h"
 struct CacheLine {
     struct bitAndFlag {
@@ -15,3 +18,5 @@ struct CacheLine {
 void init_cache();
 uint32_t cache_read(paddr_t paddr, size_t len, CacheLine* cache);
 void cache_write(paddr_t paddr, size_t len, uint32_t data, CacheLine* cache);
+
+#endif
