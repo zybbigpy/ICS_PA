87351f1b61d769c586b60aace6fd1d752f18004f
monitor <tracer@njuics.org>
2018-11-14 20:44:01 +0800

    monitor test

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -93,5 +93,18 @@ uint32_t cache_read(paddr_t paddr, size_t len, CacheLine* cache) {
 
 // write cache
 void cache_write(paddr_t paddr, size_t len, uint32_t data, CacheLine* cache) {
+    uint32_t flag = 0;
+    uint32_t setNo = 0;
+    uint32_t blockAddr = 0;
+    uint32_t lineNoBgn = 0;
+    uint32_t ret = 0;
+    uint32_t lineNoHit = 0;
+    uint32_t lineNoChosen = 0;
+    bool hitStatus = false;
+ 
+    flag = get_paddr_flag(paddr);
+    setNo = get_paddr_setNo(paddr);
+    blockAddr = get_paddr_blockAddr(paddr);
+    lineNoBgn = setNo * 8;
 
 }
