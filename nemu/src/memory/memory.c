d1b2120f26731a0d41c4ff1d52ad5b08c797bfe7
tracer-pa2018-fall <tracer@njuics.org>
2018-11-15 10:24:10 +0800

    >  nemu 151242031 mwq Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  10:24:10 up 17 min,  1 user,  load average: 0.35, 0.37, 0.23 9c8bfcb9d7ed031f122d21c75736715387048f27

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -30,7 +30,7 @@ uint32_t paddr_read(paddr_t paddr, size_t len) {
 
 void paddr_write(paddr_t paddr, size_t len, uint32_t data) {
 #ifdef CACHE_ENABLED
-	cache_write(paddr, len, data, &L1_dcache);
+	cache_write(paddr, len, data, L1_dcache);
 #else
 	hw_mem_write(paddr, len, data);
 #endif
