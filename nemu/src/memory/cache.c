59e7edb0bb4fb600088c47c177cea91fd9e2e444
tracer-pa2018-fall <tracer@njuics.org>
2018-11-15 11:27:18 +0800

    >  nemu 151242031 mwq Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  11:27:18 up  1:20,  1 user,  load average: 0.55, 0.49, 0.31 4795596c2910dcdfe97fa66c6fb83e54a9823fe9

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -51,7 +51,7 @@ uint32_t chooseLine(uint32_t lineNoBgn, CacheLine* cache) {
     }
     if(find == false) {
         srand(time(NULL));
-        linechosen = lineNoBgn+rand()%9;
+        linechosen = lineNoBgn+rand()%9-1;
     }
     return linechosen;
 }
