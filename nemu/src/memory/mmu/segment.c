1f071065aa606fcee9c6a1ce160f1a384ae2865c
monitor <tracer@njuics.org>
2018-11-25 12:25:01 +0800

    monitor test

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -21,6 +21,6 @@ void load_sreg(uint8_t sreg) {
 	// the size of segdesc is 8 bytes 
 	uint32_t laddr_segtable_fnd = laddr_segtable_bgn + (cpu.segReg[sreg].index << 3);
 	SegDesc sd;
-	memcpy(sd.val[0],laddr_segtable_fnd,4);
-	memcpy();
+	memcpy(sd.val[0], laddr_segtable_fnd, 4);
+	memcpy(sd.val[1], laddr_segtable_fnd + 4, 4);
 }
