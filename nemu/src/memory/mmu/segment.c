2ab83ec6c3c3d610ba9d4f5c9f987722f93bb29d
monitor <tracer@njuics.org>
2018-11-25 12:30:01 +0800

    monitor test

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -17,16 +17,20 @@ void load_sreg(uint8_t sreg) {
 	/* TODO: load the invisibile part of the segment register 'sreg' by reading the GDT.
 	 * The visible part of 'sreg' should be assigned by mov or ljmp already.
 	 */
+
+	// find the addr of the segtable
 	uint32_t laddr_segtable_bgn = cpu.gdtr.base;
 	// the size of segdesc is 8 bytes 
 	uint32_t laddr_segtable_fnd = laddr_segtable_bgn + (cpu.segReg[sreg].index << 3);
+	
+	// load the segdesc
 	SegDesc sd;
 	memcpy(sd.val[0], laddr_segtable_fnd, 4);
 	memcpy(sd.val[1], laddr_segtable_fnd + 4, 4);
 	// load cache part of sefreg
-	cpu.segReg[sreg].soft_use = ;
-	cpu.segReg[sreg].privilege_level =;
-	cpu.segReg[sreg].type =;
-	cpu.segReg[sreg].base =;
-	cpu.segReg[sreg].limit=;
+	cpu.segReg[sreg].soft_use = 0;
+	cpu.segReg[sreg].privilege_level =0;
+	cpu.segReg[sreg].type =0;
+	cpu.segReg[sreg].base =0;
+	cpu.segReg[sreg].limit=0;
 }
