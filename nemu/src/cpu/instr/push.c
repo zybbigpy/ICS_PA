387ecc41fea70aca6b1d15c994a78508b20ebb32
monitor <tracer@njuics.org>
2018-11-27 13:20:01 +0800

    monitor test

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -2,11 +2,13 @@
 
 static void instr_execute_1op() {
   OPERAND help;
+  opr_src.sreg = SREG_DS;
   operand_read(&opr_src);
   //  esp - 4 or esp - 2 (no esp - 1)
   //  use oprsrc.datasize is wrong for the case of imm8 
   cpu.esp -= data_size / 8;
   help.addr = cpu.esp;
+  help.sreg = SREG_DS;
   help.data_size = data_size;
   help.type = OPR_MEM;
   //for imm 8 (IA32-book)
