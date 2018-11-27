1387e3cad6305924bd9030bc304c0f9ee6e3911f
monitor <tracer@njuics.org>
2018-11-27 13:26:01 +0800

    monitor test

--- nemu/src/cpu/instr/pop.c
+++ nemu/src/cpu/instr/pop.c
@@ -5,8 +5,10 @@ static void instr_execute_1op() {
     help.addr = cpu.esp;
     help.data_size = data_size;
     help.type = OPR_MEM;
+    help.sreg = SREG_DS;
     operand_read(&help);
     opr_src.val = help.val;
+    opr_src.sreg = SREG_DS;
     operand_write(&opr_src);
     cpu.esp += data_size / 8;
 }
