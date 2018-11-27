1f62d0434fe6c8fd0ed35d8e7d10a36130156c10
monitor <tracer@njuics.org>
2018-11-27 11:05:01 +0800

    monitor test

--- nemu/src/cpu/instr/mov.c
+++ nemu/src/cpu/instr/mov.c
@@ -311,6 +311,7 @@ make_instr_func(mov_c2r_l) {
 	uint8_t modrm_byte = instr_fetch(eip + 1, 1);
 	creg_index = (modrm_byte >> 3) & 0x7;
 	gpr_index = modrm_byte & 0x7;
+	assert(creg_index == 0x0 || creg_index == 0x0);
 	switch(creg_index) {
 		case 0x0:	cpu.gpr[gpr_index].val = cpu.cr0.val;
 		//case 0x3:cpu.gpr[gpr_index].val = cpu.cr3.val;
