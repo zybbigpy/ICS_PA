b11ffa58ebc83efa73739486e7c07e9d26c02fb7
monitor <tracer@njuics.org>
2018-11-27 16:19:01 +0800

    monitor test

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -3,15 +3,15 @@
 make_instr_func(lgdt) {
     // one byte for opcode and six bytes for src
     int len  = 7;
-    uint16_t src_15 = instr_fetch(eip + 1, 2);
-    uint32_t src_16_47 = instr_fetch(eip + 3, 4);
+    uint16_t src_15 = instr_fetch(eip + 5, 2);
+    uint32_t src_16_47 = instr_fetch(eip + 1, 4);
     switch(data_size) {
         case 16:
             cpu.gdtr.limit = src_15;
             cpu.gdtr.base = src_16_47 & 0x00FFFFFF;
         case 32:
             cpu.gdtr.limit = src_15;
-            cpu.gdtr.base = src_16_47 & 0x00FFFFFF;
+            cpu.gdtr.base = src_16_47;
         default: pritf("error! in lgdt\n.");
     }
 }
