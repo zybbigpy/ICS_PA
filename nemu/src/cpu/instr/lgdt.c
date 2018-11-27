ec96f4aff9c3626b1be089d0900b29147b228fcd
monitor <tracer@njuics.org>
2018-11-27 16:13:01 +0800

    monitor test

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -1,5 +1,11 @@
 #include "cpu/instr.h"
 
 make_instr_func(lgdt) {
-    return 0;
+    // one byte for opcode and six bytes for src
+    int len  = 7;
+    switch(data_size) {
+        case 16:
+        case 32:
+        default: pritf("error! in lgdt\n.");
+    }
 }
