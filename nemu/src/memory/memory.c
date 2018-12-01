19d5e255059939ca4a096757411dc49365d42c1d
monitor <tracer@njuics.org>
2018-12-01 20:04:01 +0800

    monitor test

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -44,6 +44,7 @@ uint32_t laddr_read(laddr_t laddr, size_t len) {
 	uint32_t paddr = laddr;
 	if(cpu.cr0.pg == 1) { 
 		if((laddr & 0xfff)+ len >= 0x1000) { //page across
+			printf("page across assert!\n");
 			assert(0);
 		} else {
 			paddr = page_translate(laddr);
@@ -60,6 +61,7 @@ void laddr_write(laddr_t laddr, size_t len, uint32_t data) {
 	uint32_t paddr = laddr;
 	if(cpu.cr0.pg == 1) {// page across
 		if((laddr & 0xfff) + len >= 0x1000) {
+			printf("page across assert!\n");
 			assert(0);
 		} else {
 			paddr = page_translate(laddr);
