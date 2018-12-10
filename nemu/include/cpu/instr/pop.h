59d99a01033576691b31a5bb1bb810eebf01ad50
monitor <tracer@njuics.org>
2018-12-10 20:26:01 +0800

    monitor test

--- nemu/include/cpu/instr/pop.h
+++ nemu/include/cpu/instr/pop.h
@@ -2,5 +2,6 @@
 #define __INSTR_POP_H__
 
 make_instr_func(pop_r_v);
+make_instr_func(popa);
 
 #endif
--- nemu/include/cpu/instr/push.h
+++ nemu/include/cpu/instr/push.h
@@ -5,5 +5,6 @@ make_instr_func(push_r_v);
 make_instr_func(push_rm_v);
 make_instr_func(push_i_b);
 make_instr_func(push_i_v);
+make_instr_func(pusha);
 
 #endif 
