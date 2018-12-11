9e9e4883de5ea8703f2c45ce1b87d1829e5a1d2d
monitor <tracer@njuics.org>
2018-12-11 15:57:01 +0800

    monitor test

--- nemu/src/cpu/instr/lidt.c
+++ nemu/src/cpu/instr/lidt.c
@@ -35,20 +35,20 @@
 
 // make_instr_impl_1op(lidt, rm, v)
 
-make_instr_func(lgdt) {//Consider datasize==32 only
+make_instr_func(lidt) {//Consider datasize==32 only
 		int len=1;
         OPERAND data;
         data.type = OPR_IMM;
         data.data_size = 16;
 		len += modrm_rm(eip + 1, &data);
         operand_read(&data);
-		cpu.gdtr.limit=data.val;
+		cpu.idtr.limit=data.val;
 		//printf("addr is %x\n",data.addr);
         data.data_size = 32;
 		data.addr+=2;
         operand_read(&data);
-		print_asm_1("lgdt", "", 2, &data);
-		cpu.gdtr.base=data.val;
+		print_asm_1("lidt", "", 2, &data);
+		cpu.idtr.base=data.val;
 		//printf("GDTR is %x,%x\n",cpu.GDTR.base,cpu.GDTR.limit);
 		return len;
 }
