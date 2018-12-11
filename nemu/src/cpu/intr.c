a124e204800cd8777e33f861d6acc577ae9791cb
monitor <tracer@njuics.org>
2018-12-11 11:34:01 +0800

    monitor test

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -7,6 +7,8 @@ void raise_intr(uint8_t intr_no) {
 #ifdef IA32_INTR
 	//printf("Please implement raise_intr()");
 	//assert(0);
+	laddr_t idt_entry = cpu.idtr.base + intr_no *sizeof(GateDesc);
+
 #endif
 }
 
