99471f7c6d0641c30ef991a1578296df4b0032e0
monitor <tracer@njuics.org>
2018-12-11 20:36:01 +0800

    monitor test

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -1,19 +1,18 @@
 #include "cpu/instr.h"
 
-make_instr_func(lgdt) {//Consider datasize==32 only
-		int len=1;
-        OPERAND data;
-        data.type = OPR_IMM;
-        data.data_size = 16;
-		len += modrm_rm(eip + 1, &data);
-        operand_read(&data);
-		cpu.gdtr.limit=data.val;
-		//printf("addr is %x\n",data.addr);
-        data.data_size = 32;
-		data.addr+=2;
-        operand_read(&data);
-		print_asm_1("lgdt", "", 2, &data);
-		cpu.gdtr.base=data.val;
-		//printf("GDTR is %x,%x\n",cpu.GDTR.base,cpu.GDTR.limit);
-		return len;
+make_instr_func(lgdt) { 
+	int len = 1;
+	OPERAND data;
+	data.type = OPR_IMM;
+	data.data_size = 16;
+	len += modrm_rm(eip + 1, &data);
+	operand_read(&data);
+	cpu.gdtr.limit = data.val;
+	//printf("addr is %x\n",data.addr);
+	data.data_size = 32;
+	data.addr += 2;
+	operand_read(&data);
+	print_asm_1("lgdt", "", 2, &data);
+	cpu.gdtr.base = data.val;
+	return len;
 }
