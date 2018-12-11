524b6560d64ff438dfab94bf1132ad828c0a43af
monitor <tracer@njuics.org>
2018-12-11 15:37:01 +0800

    monitor test

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -16,25 +16,32 @@ void raise_intr(uint8_t intr_no) {
 #ifdef IA32_INTR
 	//printf("Please implement raise_intr()");
 	//assert(0);
-	push(eflags.val, data_size, help1)
-	push(cs.val, data_size, help2)
-	push(eip, data_size, help3)
+		union{
+		GateDesc Gatedesc;
+		uint32_t val[2];
+	} GD;
+	
+	//push 
+	cpu.esp-=4;
+	vaddr_write(cpu.esp,SREG_SS,4,cpu.eflags.val);
+	//printf("esp\n");
+	cpu.esp-=2;
+	vaddr_write(cpu.esp,SREG_SS,2,cpu.cs.val);
+	cpu.esp-=4;
+	vaddr_write(cpu.esp,SREG_SS,4,cpu.eip);
+	//set flag
+	cpu.eflags.IF=0;
+	cpu.eflags.TF=0;
 
-	//cpu.eflags.IF = 0;
-	//cpu.eflags.TF = 0;
-
-	laddr_t idt_entry = cpu.idtr.base + intr_no *sizeof(GateDesc);
-	GateDesc idt;
-	idt.val[0] = laddr_read(idt_entry, 4);
-	idt.val[1] = laddr_read(idt_entry + 4, 4);
-
-	assert(idt.present == 1);
-	if(idt.type == 0xe) {
-		cpu.eflags.IF = 0;
-	}
-	cpu.eip = (idt.offset_31_16 << 16) + idt.offset_15_0;
-	cpu.cs.val = idt.selector;
-	load_sreg(SREG_CS);
+	//jmp
+	GD.val[0]=laddr_read(cpu.idtr.base+intr_no*8,4);
+	GD.val[1]=laddr_read(cpu.idtr.base+intr_no*8+4,4);
+	cpu.eip=GD.Gatedesc.offset_15_0+(GD.Gatedesc.offset_31_16<<16);
+	//cpu.CS.val=GD.Gatedesc.type;
+	//load_sreg(SREG_CS);
+	//printf("IDTR is %x\n",cpu.IDTR.base);
+	
+	return;
 
 #endif
 }
