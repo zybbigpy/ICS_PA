21b5b189734d6da615908acec66c59631957c0de
monitor <tracer@njuics.org>
2018-12-11 11:07:01 +0800

    monitor test

--- nemu/src/cpu/instr/iret.c
+++ nemu/src/cpu/instr/iret.c
@@ -9,8 +9,7 @@ help.sreg = SREG_SS;        \
 operand_read(&help);        \
 cpu.reg = help.val;         \
 cpu.esp += data_size / 8;   \
+
 make_instr_func(iret) {
-    uint8_t no = instr_fetch(eip + 1, 1);
-    raise_sw_intr(no);
     return 0;
 }
