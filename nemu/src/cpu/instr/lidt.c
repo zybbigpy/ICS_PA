ce2e65af31212369538ae7fb235b10224ac1217a
monitor <tracer@njuics.org>
2018-12-11 15:54:01 +0800

    monitor test

--- nemu/src/cpu/instr/lidt.c
+++ nemu/src/cpu/instr/lidt.c
@@ -21,16 +21,34 @@
 //     return len;
 // }
 
-static void instr_execute_1op() {
-	// printf("lidt..\n");
-	laddr_t mem_addr = opr_src.val;
-	// printf("mem_addr = 0x%x\n", mem_addr);
-	cpu.idtr.limit = laddr_read(mem_addr, 2) & 0xffff;
-	cpu.idtr.base = laddr_read(mem_addr + 2, 4) & 0xffffffff;
-	if (data_size == 16)
-		cpu.idtr.base &= 0x00ffffff;
-	// printf("idtr: limit = 0x%x, base = 0x%x\n", cpu.idtr.limit, cpu.idtr.base);
+// static void instr_execute_1op() {
+// 	// printf("lidt..\n");
+// 	laddr_t mem_addr = opr_src.val;
+// 	// printf("mem_addr = 0x%x\n", mem_addr);
+// 	cpu.idtr.limit = laddr_read(mem_addr, 2) & 0xffff;
+// 	cpu.idtr.base = laddr_read(mem_addr + 2, 4) & 0xffffffff;
+// 	if (data_size == 16)
+// 		cpu.idtr.base &= 0x00ffffff;
+// 	// printf("idtr: limit = 0x%x, base = 0x%x\n", cpu.idtr.limit, cpu.idtr.base);
 
+// }
+
+// make_instr_impl_1op(lidt, rm, v)
+
+make_instr_func(lgdt) {//Consider datasize==32 only
+		int len=1;
+        OPERAND data;
+        data.type = OPR_IMM;
+        data.data_size = 16;
+		len += modrm_rm(eip + 1, &data);
+        operand_read(&data);
+		cpu.gdtr.limit=data.val;
+		//printf("addr is %x\n",data.addr);
+        data.data_size = 32;
+		data.addr+=2;
+        operand_read(&data);
+		print_asm_1("lgdt", "", 2, &data);
+		cpu.gdtr.base=data.val;
+		//printf("GDTR is %x,%x\n",cpu.GDTR.base,cpu.GDTR.limit);
+		return len;
 }
-
-make_instr_impl_1op(lidt, rm, v)
