d43357dfdb9e10ab0175183a6b89b5b652f8f841
monitor <tracer@njuics.org>
2018-12-11 13:12:01 +0800

    monitor test

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -44,8 +44,12 @@ uint32_t laddr_read(laddr_t laddr, size_t len) {
 	uint32_t paddr = laddr;
 	if(cpu.cr0.pg == 1) { 
 		if((laddr & 0xfff)+ len > 0x1000) { //page across
-			printf("page across assert!\n");
-			assert(0);
+			//printf("page across assert!\n");
+			//assert(0);
+			uint32_t res = 0;
+			while(len--)
+				res = (res << 8) | laddr_read(paddr+len-1, 1);
+			return res;
 		} else {
 			paddr = page_translate(laddr);
 		}	
