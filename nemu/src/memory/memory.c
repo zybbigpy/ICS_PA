c09e53d7fe0cb2d6ea540f5a0025f5c0b41f1be0
tracer-pa2018-fall <tracer@njuics.org>
2018-12-23 10:27:31 +0800

    >  clean 151242031 mwq Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  10:27:31 up 36 min,  1 user,  load average: 0.16, 0.33, 0.32 b82ca6a8a2335d26bc956b1b3bee630a2086f876

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -69,12 +69,12 @@ void laddr_write(laddr_t laddr, size_t len, uint32_t data) {
 	if(cpu.cr0.pg == 1) {
 		if((laddr & 0xfff) + len > 0x1000) {
 			// page across
-			//printf("page across assert!\n");
-			//assert(0);
-			uint32_t temp = data;
-			for(uint32_t i = 0; i < len; ++i) {
-				laddr_write(paddr + i, 1, (temp & 0xff));
-				temp >>= 8;
+			// printf("page across assert!\n");
+			// assert(0);
+			// uint32_t temp = data;
+			for(int i = 0; i != len; i++) {
+				laddr_write(paddr + i, 1, (data & 0xff));
+				data >>= 8;
 			}
 			return ;
 		} else {
