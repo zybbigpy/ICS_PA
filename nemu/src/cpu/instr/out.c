2ce219d33910e6b9f23332c29e96ccaec6c2acbd
monitor <tracer@njuics.org>
2018-12-23 20:49:01 +0800

    monitor test

--- nemu/src/cpu/instr/out.c
+++ nemu/src/cpu/instr/out.c
@@ -2,13 +2,13 @@
 #include "device/port_io.h"
 
 make_instr_func(out_b) {
-   pio_write(cpu.edx & 0xffff, 1, cpu.eax);
+   pio_write(cpu.edx & 0xffff, 1, cpu.eax & 0xff);
    return 1;
 }
 
 make_instr_func(out_v) {
     if(data_size == 16) {
-        cpu.eax = pio_read(cpu.edx & 0xffff, 2);
+        
     }
     else if(data_size == 32) {
         cpu.eax = pio_read(cpu.edx & 0xffff, 4);
