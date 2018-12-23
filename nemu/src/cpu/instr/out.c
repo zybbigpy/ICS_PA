700593b7423be820339a9f78049f6a29bf5f5245
monitor <tracer@njuics.org>
2018-12-23 20:43:01 +0800

    monitor test

--- nemu/src/cpu/instr/out.c
+++ nemu/src/cpu/instr/out.c
@@ -2,11 +2,11 @@
 #include "device/port_io.h"
 
 make_instr_func(out_b) {
-   cpu.eax = (uint8_t) pio_read(cpu.edx, 1);
+   cpu.eax = (uint8_t) pio_read(cpu.edx & 0xffff, 1);
    return 1;
 }
 
 make_instr_func(out_v) {
-    cpu.eax = pio_read(cpu.edx, 4);
+    cpu.eax = pio_read(cpu.edx & 0xffff, 4);
     return 1;
 }
