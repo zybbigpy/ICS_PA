52ea18422c7250858b22d350560cd555462cf869
monitor <tracer@njuics.org>
2018-12-23 10:02:01 +0800

    monitor test

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -9,11 +9,14 @@ make_instr_func(lgdt) {
 	len += modrm_rm(eip + 1, &rm1);
 	operand_read(&rm1);
 	cpu.gdtr.limit = rm1.val;
-	
-	data.data_size = 32;
-	data.addr += 2;
-	operand_read(&data);
-	cpu.gdtr.base = data.val;
+
+	OPERAND rm2;
+	rm2.data_size = 32;
+	rm2.addr = rm1.addr + 2;
+	rm2.sreg = rm1.sreg;
+	rm2.type = OPR_IMM;
+	operand_read(&rm2);
+	cpu.gdtr.base = rm2.val;
 	// print_asm_1("lgdt", "", 2, &data);
 	return len;
 }
