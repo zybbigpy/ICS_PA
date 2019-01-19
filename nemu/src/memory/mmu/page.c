83020402f42f5e0345338b421d09277fe5d5ad08
monitor <tracer@njuics.org>
2019-01-19 16:12:01 +0800

    monitor test

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -4,22 +4,22 @@
 // translate from linear address to physical address
 paddr_t page_translate(laddr_t laddr) {
 #ifndef TLB_ENABLED
-	printf("\nPlease implement page_translate()\n");
-	assert(0);
-	uint32_t DIR = laddr >> 22;
-	uint32_t PAGE = (laddr >> 12) & 0x3ff;
-	uint32_t OFFSET = laddr & 0xfff;
-	uint32_t paddr = 0;
-	uint32_t page_dir_base_addr = cpu.cr3.pdbr << 12;
-	PDE pde;
-	memcpy(&pde, hw_mem + page_dir_base_addr + DIR * 4, 4);
-	assert(pde.present == 1);
-	uint32_t page_tbl_base_addr = pde.page_frame << 12;
-	PTE pte;
-	memcpy(&pte, hw_mem + page_tbl_base_addr + PAGE * 4, 4);
-	assert(pte.present == 1);
-	paddr = (pte.page_frame << 12) + OFFSET;
-	return paddr;
+	//printf("\nPlease implement page_translate()\n");
+	//assert(0);
+	// uint32_t DIR = laddr >> 22;
+	// uint32_t PAGE = (laddr >> 12) & 0x3ff;
+	// uint32_t OFFSET = laddr & 0xfff;
+	// uint32_t paddr = 0;
+	// uint32_t page_dir_base_addr = cpu.cr3.pdbr << 12;
+	// PDE pde;
+	// memcpy(&pde, hw_mem + page_dir_base_addr + DIR * 4, 4);
+	// assert(pde.present == 1);
+	// uint32_t page_tbl_base_addr = pde.page_frame << 12;
+	// PTE pte;
+	// memcpy(&pte, hw_mem + page_tbl_base_addr + PAGE * 4, 4);
+	// assert(pte.present == 1);
+	// paddr = (pte.page_frame << 12) + OFFSET;
+	// return paddr;
 	PDE pde;
 	PTE pte;
 	paddr_t paddr = 0;
