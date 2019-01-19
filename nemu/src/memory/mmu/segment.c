cb06d1c1a8e419103af0af6fe368d8aa9cda2604
tracer-pa2018-fall <tracer@njuics.org>
2019-01-19 15:15:51 +0800

    >  nemu 151242031 mwq Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  15:15:51 up 44 min,  1 user,  load average: 0.14, 0.22, 0.25 18065ca7ac97a7799633159057412e4a4b68c77

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -17,7 +17,7 @@ void load_sreg(uint8_t sreg) {
 	/* TODO: load the invisibile part of the segment register 'sreg' by reading the GDT.
 	 * The visible part of 'sreg' should be assigned by mov or ljmp already.
 	 */
-	
+
 	// find the addr of the segtable
 	uint32_t laddr_segtable_bgn = cpu.gdtr.base;
 	// the size of segdesc is 8 bytes 
@@ -26,23 +26,30 @@ void load_sreg(uint8_t sreg) {
 	uint32_t laddr_segtable_fnd = laddr_segtable_bgn + laddr_segtable_offset;
 	
 	// load the segdesc
-	SegDesc sd;
-	sd.val[0] = laddr_read(laddr_segtable_fnd, 4);
-	sd.val[1] = laddr_read(laddr_segtable_fnd + 4, 4);
+	// SegDesc sd;
+	// sd.val[0] = laddr_read(laddr_segtable_fnd, 4);
+	// sd.val[1] = laddr_read(laddr_segtable_fnd + 4, 4);
 
-	// load cache part of segreg according to the segdesc
+	// // load cache part of segreg according to the segdesc
 	
-	cpu.segReg[sreg].soft_use = sd.soft_use;
-	cpu.segReg[sreg].privilege_level = sd.soft_use;
-	cpu.segReg[sreg].type = sd.type;
-	cpu.segReg[sreg].base = (sd.base_31_24 << 24) + (sd.base_23_16 << 16) 
-							+ sd.base_15_0;
-	cpu.segReg[sreg].limit = (sd.limit_19_16 << 16) + sd.limit_15_0;
-
+	// cpu.segReg[sreg].soft_use = sd.soft_use;
+	// cpu.segReg[sreg].privilege_level = sd.soft_use;
+	// cpu.segReg[sreg].type = sd.type;
+	// cpu.segReg[sreg].base = (sd.base_31_24 << 24) + (sd.base_23_16 << 16) 
+	// 						+ sd.base_15_0;
+	// cpu.segReg[sreg].limit = (sd.limit_19_16 << 16) + sd.limit_15_0;
+	SegDesc segdesc;
+	segdesc.val[0]=laddr_read(cpu.gdtr.base+cpu.segReg[sreg].index*sizeof(SegDesc),4);
+	segdesc.val[1]=laddr_read(cpu.gdtr.base+cpu.segReg[sreg].index*sizeof(SegDesc)+4,4);
+	//printf("%d's index is %d\n",sreg,cpu.segReg[sreg].index);
+	//printf("segdesc=%x%x\n",segdesc.val[0],segdesc.val[1]);
+	assert(segdesc.present==1);
+	cpu.segReg[sreg].base=segdesc.base_15_0+(segdesc.base_23_16<<16)+(segdesc.base_31_24<<24);
+	cpu.segReg[sreg].limit=segdesc.limit_15_0+(segdesc.limit_19_16<<16);
 	// for debug use
-	if(cpu.cr0.pe == 0) {
-		assert(sd.granularity == 1);
-		assert(cpu.segReg[sreg].base == 0x0);
-		assert(cpu.segReg[sreg].limit == 0xfffff);
-	}
+	// if(cpu.cr0.pe == 0) {
+	// 	assert(sd.granularity == 1);
+	// 	assert(cpu.segReg[sreg].base == 0x0);
+	// 	assert(cpu.segReg[sreg].limit == 0xfffff);
+	// }
 }
