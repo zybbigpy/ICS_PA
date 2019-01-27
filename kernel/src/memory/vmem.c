99d0a3fa66b7b7baad83b032c37a6a13ec548bfe
monitor <tracer@njuics.org>
2019-01-27 19:14:01 +0800

    monitor test

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -16,7 +16,7 @@ void create_video_mapping() {
 	 * some page tables to create this mapping.
 	 */
 
-	panic("please implement me");
+	//panic("please implement me");
 }
 
 void video_mapping_write_test() {
