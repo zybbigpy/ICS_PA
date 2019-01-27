bd0331f4ceccaa32ad938ff0b0e3b464d22c07e0
monitor <tracer@njuics.org>
2019-01-27 19:08:01 +0800

    monitor test

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -31,10 +31,7 @@ void load_sreg(uint8_t sreg) {
 	sd.val[0] = laddr_read(laddr_segtable_fnd, 4);
 	sd.val[1] = laddr_read(laddr_segtable_fnd + 4, 4);
 
-	// // load cache part of segreg according to the segdesc
-	printf("cpu cr0 pe = %d \n", cpu.cr0.pe);
-	printf("sd.present = %d \n", sd.present);
-	// assert(sd.present==1);
+	assert(sd.present == 1);
 	cpu.segReg[sreg].soft_use = sd.soft_use;
 	cpu.segReg[sreg].privilege_level = sd.soft_use;
 	cpu.segReg[sreg].type = sd.type;
