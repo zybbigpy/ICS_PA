d818d0cb81d7d9648a43ec78ac10dcbdac7994ef
monitor <tracer@njuics.org>
2019-01-27 19:29:02 +0800

    monitor test

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -30,24 +30,31 @@ make_instr_func(call_near) {
 }
 
 make_instr_func(call_near_indirect) {
-    //push eip
-    OPERAND help;
-    cpu.esp -= data_size / 8;               //maybe 2
-    help.type = OPR_MEM;
-    help.data_size = data_size;
-    help.val = cpu.eip + 1 + data_size / 8;
-    help.addr = cpu.esp;  
-    help.sreg = SREG_SS; 
-    operand_write(&help);                   //write will do eip(ip) -> stack
+    // //push eip
+    // OPERAND help;
+    // cpu.esp -= data_size / 8;               //maybe 2
+    // help.type = OPR_MEM;
+    // help.data_size = data_size;
+    // help.val = cpu.eip + 1 + data_size / 8;
+    // help.addr = cpu.esp;  
+    // help.sreg = SREG_SS; 
+    // operand_write(&help);                   //write will do eip(ip) -> stack
 
-    //realize like jmp_near_indirect
-    OPERAND rm;
-    rm.data_size = data_size;
-    rm.sreg = SREG_CS;
+    // //realize like jmp_near_indirect
+    // OPERAND rm;
+    // rm.data_size = data_size;
+    // rm.sreg = SREG_CS;
+    // int len = 1;
+    // len += modrm_rm(eip + 1, &rm);          // write addr and type for opr_rm
+    // operand_read(&rm);
+    // cpu.eip = rm.val;
     int len = 1;
-    len += modrm_rm(eip + 1, &rm);          // write addr and type for opr_rm
-    operand_read(&rm);
-    cpu.eip = rm.val;
-
+    OPERAND rel;
+    cpu.esp -= 4;
+    len += modrm_rm(eip + 1, &rel);
+    rel.data_size = data_size;
+    operand_read(&rel);
+    vaddr_write(cpu.esp, SREG_SS, 4, eip + len);
+    cpu.eip = rel.val;
     return 0;
 }
