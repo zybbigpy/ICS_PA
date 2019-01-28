16757f80bd128c1626cc1877592bf8bba226184c
monitor <tracer@njuics.org>
2019-01-28 22:27:01 +0800

    monitor test

--- kernel/src/fs/fs.c
+++ kernel/src/fs/fs.c
@@ -117,72 +117,86 @@ void ide_write(uint8_t *, uint32_t, uint32_t);
 
 /* Please implement a simple file system here */
 
-int fs_open(const char *pathname, int flags) {
+int fs_open(const char *pathname, int flags)
+{
 	//panic("Please implement fs_open at fs.c");
-	for(int i=0;i<NR_FILES;i++){
-		if(strcmp(pathname,file_table[i].name)==0){
-			files[i+3].used=1;
-			files[i+3].offset=0;
+	for (int i = 0; i < NR_FILES; i++)
+	{
+		if (strcmp(pathname, file_table[i].name) == 0)
+		{
+			files[i + 3].used = 1;
+			files[i + 3].offset = 0;
 			//Log("file \"%s\" opend",pathname);
-			return i+3;
+			return i + 3;
 		}
 	}
 	assert(0);
 	return -1;
 }
 
-size_t fs_read(int fd, void *buf, size_t len) {
+size_t fs_read(int fd, void *buf, size_t len)
+{
 	assert(fd > 2);
 	assert(files[fd].used);
 	//panic("Please implement fs_read at fs.c");
-	uint32_t real_len=len;
+	uint32_t real_len = len;
 	//Log("Read from %s:%x %x bytes",file_table[fd-3].name,files[fd].offset,len);
-	if(files[fd].offset+len>=file_table[fd-3].size) real_len=file_table[fd-3].size-files[fd].offset-1;
-	if(real_len==0) return 0;
-	if(files[fd].offset>=file_table[fd-3].size) return 0;
-	memset(buf+real_len,0,len-real_len);
-	ide_read(buf,file_table[fd-3].disk_offset+files[fd].offset,real_len);
-	files[fd].offset+=real_len;
+	if (files[fd].offset + len >= file_table[fd - 3].size)
+		real_len = file_table[fd - 3].size - files[fd].offset - 1;
+	if (real_len == 0)
+		return 0;
+	if (files[fd].offset >= file_table[fd - 3].size)
+		return 0;
+	memset(buf + real_len, 0, len - real_len);
+	ide_read(buf, file_table[fd - 3].disk_offset + files[fd].offset, real_len);
+	files[fd].offset += real_len;
 	//Log("offset of %s is %x,return %x",file_table[fd-3].name,files[fd].offset,real_len);
 	return real_len;
 }
 
-size_t fs_write(int fd, void *buf, size_t len) {
+size_t fs_write(int fd, void *buf, size_t len)
+{
 	assert(fd <= 2);
 #ifdef HAS_DEVICE_SERIAL
 	int i;
 	extern void serial_printc(char);
-	for(i = 0; i < len; i ++) {
-		serial_printc( ((char *)buf)[i] );
+	for (i = 0; i < len; i++)
+	{
+		serial_printc(((char *)buf)[i]);
 	}
 #else
-	asm volatile(".byte 0x82" : "=a" (len) : "a"(4), "b"(fd), "c"(buf), "d"(len));
+	asm volatile(".byte 0x82"
+				 : "=a"(len)
+				 : "a"(4), "b"(fd), "c"(buf), "d"(len));
 #endif
 
 	return len;
 }
 
-off_t fs_lseek(int fd, off_t offset, int whence) {
+off_t fs_lseek(int fd, off_t offset, int whence)
+{
 	//panic("Please implement fs_lseek at fs.c");
 	//Log("seek!");
-	switch(whence){
-		case SEEK_SET:
-			files[fd].offset=offset;
-			return files[fd].offset;
-		case SEEK_CUR:
-			files[fd].offset+=offset;
-			return files[fd].offset;
-		case SEEK_END:
-			files[fd].offset=file_table[fd-3].size+offset;
-			return files[fd].offset;
+	switch (whence)
+	{
+	case SEEK_SET:
+		files[fd].offset = offset;
+		return files[fd].offset;
+	case SEEK_CUR:
+		files[fd].offset += offset;
+		return files[fd].offset;
+	case SEEK_END:
+		files[fd].offset = file_table[fd - 3].size + offset;
+		return files[fd].offset;
 	}
 	return -1;
 }
 
-int fs_close(int fd) {
+int fs_close(int fd)
+{
 	//panic("Please implement fs_close at fs.c");
-	assert(fd<NR_FILES+3);
-	files[fd].used=0;
-	files[fd].offset=0;
+	assert(fd < NR_FILES + 3);
+	files[fd].used = 0;
+	files[fd].offset = 0;
 	return 0;
 }
